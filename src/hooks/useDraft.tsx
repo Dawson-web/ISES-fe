85ca15391415e82fb3892dcafbfcbc2794bd8103
Dawson-web <3096567831@qq.com>
2025-07-18 23:09:10 +0800

    refactor: 移除未使用的删除草稿功能，优化草稿保存逻辑，添加草稿检查和导入功能

--- src/hooks/useDraft.tsx
+++ src/hooks/useDraft.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect, useCallback } from "react";
-import { getDraft, saveDraft, deleteDraft } from "@/utils/indexDB";
-import { useBlocker } from "react-router-dom";
+import { getDraft, saveDraft } from "@/utils/indexDB";
+import { useLocation } from "react-router-dom";
+
 export function useDraft({
   getEditorContent,
   setEditorContent,
@@ -13,7 +14,9 @@ export function useDraft({
   setOtherFields?: (fields: any) => void;
 }) {
   const [hasDraft, setHasDraft] = useState(false);
-  const isSaved = useRef(false);
+  const pathname = useLocation().pathname;
+
+// 检查是否存在草稿
   useEffect(() => {
     const CheckDraft = async () => {
       const draft = await getDraft();
@@ -23,28 +26,36 @@ export function useDraft({
     };
     CheckDraft();
   }, []);
-  useEffect(() => {
-    const handleRouteChange = async () => {
-      if (window.confirm("保存草稿?")) {
-        await saveDraft({
-          content: getEditorContent(),
-          ...getOtherFields(),
-        });
-      }
-    };
 
-    const handleBeforeUnload = (e) => {
+  const handleRouteChange = async () => {
+    if (window.confirm("保存草稿?")) {
+      await saveDraft({
+        content: getEditorContent(),
+        ...getOtherFields?.(),
+      });
+    }
+  };
+
+  // 保存草稿
+  useEffect(() => {
+    const handleBeforeUnload = (e: any) => {
       e.preventDefault();
       e.returnValue = "";
     };
-    const unblock = history.block(handleRouteChange);
     window.addEventListener("beforeunload", handleBeforeUnload);
 
     return () => {
-      unblock();
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, [getEditorContent, setEditorContent, getOtherFields]);
+
+  useEffect(() => {
+    return () => {
+      handleRouteChange();
+    };
+  }, [pathname]);
+
+  // 导入草稿
   const importDraft = useCallback(async () => {
     const draft = await getDraft();
     if (draft) {
