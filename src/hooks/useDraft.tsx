41631b78589cb97b1bd16da0946d3d485cd0be4c
lpwo443 <1422885400@qq.com>
2025-07-24 18:43:50 +0800

    草稿功能完善

---
+++ src/hooks/useDraft.tsx
@@ -0,0 +1,172 @@
+import { useEffect, useCallback, useRef } from "react";
+import { getDraft, saveDraft, delDraft, getDraftById } from "@/utils/indexDB";
+import { useLocation } from "react-router-dom";
+import { toast } from "sonner";
+import { Message } from "@arco-design/web-react";
+import isEqual from "lodash.isequal";
+import { get } from "http";
+export function useDraft({
+  getEditorContent,
+  setEditorContent,
+  getOtherFields,
+  setOtherFields,
+}: {
+  getEditorContent: () => any;
+  setEditorContent: (data: any) => void;
+  getOtherFields?: () => any; // 获取其他字段（如标题、标签等）
+  setOtherFields?: (fields: any) => void;
+}) {
+  const pathname = useLocation().pathname;
+  const isSaved = useRef(false);
+  const lastValues = useRef<any>(null);
+  const lastId = useRef<number | null>(null);
+  //草稿对比函数
+  const SameValue = () => {
+    const currentValues = {
+      content: getEditorContent(),
+      ...getOtherFields?.(),
+    };
+    //console.log(isEqual(lastValues, currentValues));
+    if (lastValues.current) {
+      return (
+        JSON.stringify(lastValues.current) === JSON.stringify(currentValues)
+      );
+    }
+  };
+  //路由切换函数
+  const handleRouteChange = async () => {
+    const content = getEditorContent();
+    const Paragraph = content?.content?.[0];
+    const Text = Paragraph?.content?.[0]?.text;
+    //if (Text === "开始你的创作..." || !Text) {
+    //  Message.info("内容不能为空，无法保存草稿");
+    //  return;
+    // }
+    //if (SameValue()) {
+    //  Message.info("内容未发生变化，无需保存");
+    // return;
+    //}
+    const id = lastId.current ?? Date.now();
+    if (
+      !window.location.pathname.endsWith("/edit") &&
+      !isSaved.current &&
+      !SameValue() &&
+      Text &&
+      Text !== "开始你的创作..."
+    ) {
+      toast("您是否要保存草稿？", {
+        action: (
+          <div className="flex gap-2">
+            <button
+              onClick={async () => {
+                await saveDraft({
+                  id,
+                  content: getEditorContent(),
+                  ...getOtherFields?.(),
+                });
+                isSaved.current = false;
+                lastValues.current = {
+                  content: getEditorContent(),
+                  ...getOtherFields?.(),
+                };
+                lastId.current = null;
+              }}
+              className="px-3 py-1 bg-blue-500 text-white rounded"
+            >
+              保存
+            </button>
+            <button
+              onClick={async () => {
+                await deleteDraft(id);
+              }}
+              className="px-3 py-1 bg-gray-300 rounded"
+            >
+              不保存
+            </button>
+          </div>
+        ),
+        duration: 2000,
+      });
+    }
+  };
+  // 检查是否存在草稿
+  //useEffect(() => {
+  //  const CheckDraft = async () => {
+  //    const draft = await getDraft();
+  //   if (draft) {
+  //     setHasDraft(true);
+  //   }
+  // };
+  // CheckDraft();
+  //}, []);
+
+  //刷新页面提示
+  useEffect(() => {
+    const handleBeforeUnload = (e: any) => {
+      e.preventDefault();
+      e.returnValue = "";
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [getEditorContent, setEditorContent]);
+  //切换路由提示
+  useEffect(() => {
+    return () => {
+      handleRouteChange();
+    };
+  }, [pathname]);
+  //获取所有草稿
+  const fetchAllDrafts = useCallback(async () => {
+    const drafts = await getDraft();
+    return drafts;
+  }, []);
+  // 导入草稿
+  const importDraft = useCallback(
+    async (Id: number) => {
+      const draft = await getDraftById(Id);
+      console.log(draft);
+      const { id, content, ...otherFields } = draft;
+      if (draft) {
+        setEditorContent(content);
+        setOtherFields?.(otherFields as any);
+        lastValues.current = {
+          content,
+          ...otherFields,
+        };
+        lastId.current = id;
+        console.log(lastValues.current);
+      }
+    },
+    [setEditorContent, setOtherFields]
+  );
+  //删除草稿
+  const deleteDraft = useCallback(async (id: number) => {
+    await delDraft(id);
+  }, []);
+  //单独保存草稿的功能
+  const toSaveDraft = useCallback(async () => {
+    const content = getEditorContent();
+    const Paragraph = content?.content?.[0];
+    const Text = Paragraph?.content?.[0]?.text;
+    if (Text === "开始你的创作..." || !Text) {
+      Message.info("内容不能为空，无法保存草稿");
+      return;
+    }
+    if (SameValue()) {
+      Message.info("内容未发生变化，无需保存");
+      return;
+    }
+    const id = lastId.current ?? Date.now();
+    await saveDraft({
+      id,
+      content: getEditorContent(),
+      ...getOtherFields?.(),
+    });
+    isSaved.current = true;
+    lastValues.current = { content: getEditorContent(), ...getOtherFields?.() };
+    lastId.current = null;
+  }, [getEditorContent, getOtherFields]);
+  return { importDraft, deleteDraft, fetchAllDrafts, toSaveDraft, lastId };
+}
--- src/pages/navigator/articles/edit/index.tsx
+++ src/pages/navigator/articles/edit/index.tsx
@@ -1,79 +1,156 @@
-import { useState } from 'react';
-import { Input, Button, Message, Avatar, Tag, Select } from '@arco-design/web-react';
-import { 
-  IconSave,
-  IconSend,
-} from '@arco-design/web-react/icon';
+import { useState } from "react";
+import {
+  Input,
+  Button,
+  Message,
+  Avatar,
+  Tag,
+  Select,
+  Modal,
+  List,
+} from "@arco-design/web-react";
+import { IconSave, IconSend } from "@arco-design/web-react/icon";
 // import { useSearchParams } from 'react-router-dom';
 
-import './style.css';
+import "./style.css";
 // import { createArticle } from '@/service/article';
-import { IArticleForm } from '@/types/article';
-import {  createArticleApi } from '@/service/article';
-import IeseEditor, { useAritcleEditor } from '@/components/editor';
-import MenuBar from '@/components/editor/MenuBar';
+import { IArticleForm } from "@/types/article";
+import { createArticleApi } from "@/service/article";
+import IeseEditor, { useAritcleEditor } from "@/components/editor";
+import MenuBar from "@/components/editor/MenuBar";
+import { useDraft } from "@/hooks/useDraft";
 
 const CATEGORY = [
   {
-    label: '日常',
-    value: 'life'
+    label: "日常",
+    value: "life",
   },
   {
-    label: '校园',
-    value: 'campus'
+    label: "校园",
+    value: "campus",
   },
   {
-    label: '公司爆料',
-    value: 'company'
-  }
-]
+    label: "公司爆料",
+    value: "company",
+  },
+];
 
 export const CONTENT_TYPE = {
-  'life': ['动态','技术','分享'],
-  'campus': ['内推'],
-  'company': ['信息']
-}
+  life: ["动态", "技术", "分享"],
+  campus: ["内推"],
+  company: ["信息"],
+};
 
 export default function ArticleEditPage() {
-   const [form, setForm] = useState<IArticleForm>({
-    title: '',
-    content: '',
-    type: '技术',
-    cover: '',
+  const [form, setForm] = useState<IArticleForm>({
+    title: "",
+    content: "",
+    type: "技术",
+    cover: "",
     category: undefined,
     contentType: undefined,
     tags: [],
-    excerpt: ''
+    excerpt: "",
   });
+
   const [isDraft] = useState(true);
-  const editor = useAritcleEditor('');
+  const editor = useAritcleEditor("");
+  const [draftModalVisible, setDraftModalVisible] = useState(false);
+  const [tag, setTag] = useState(1);
+  const [drafts, setDrafts] = useState<
+    Array<{
+      id: number;
+      content: any;
+      title: string;
+      category: string;
+      excerpt: string;
+      contentType: string;
+      tags: string[];
+    }>
+  >([]);
+  //调用钩子
+  const { importDraft, deleteDraft, fetchAllDrafts, toSaveDraft, lastId } =
+    useDraft({
+      getEditorContent: () => editor?.getJSON(),
+      setEditorContent: (content) => editor?.commands.setContent(content),
+      getOtherFields: () => ({
+        title: form.title,
+        tags: form.tags,
+        category: form.category,
+        contentType: form.contentType,
+        excerpt: form.excerpt,
+      }),
+      setOtherFields: (fields: any) => {
+        setForm((prev) => ({
+          ...prev,
+          title: fields.title || prev.title,
+          tags: fields.tags || prev.tags,
+          category: fields.category || prev.category,
+          contentType: fields.contentType || prev.contentType,
+          excerpt: fields.excerpt || prev.excerpt,
+        }));
+      },
+    });
+  //模块框功能
 
+  const handleOpenDraftModal = async () => {
+    setDraftModalVisible(true);
+    const draft = await fetchAllDrafts();
+    setDrafts(draft);
+  };
 
+  const handleImportDraft = (id: number) => {
+    importDraft(id);
+    setDraftModalVisible(false);
+  };
+
+  const handleDeleteDraft = (id: number) => {
+    Modal.confirm({
+      title: "确认删除？",
+      content: "删除后将无法恢复",
+      onOk: () => {
+        //await deleteDraft(id);
+        //setDrafts(drafts.filter((draft) => draft.id !== id));
+        //setDraftModalVisible(false);
+        try {
+          deleteDraft(id);
+          setDrafts(drafts.filter((draft) => draft.id !== id)); // 更新列表
+        } catch (e) {
+          console.error("删除失败", e);
+          Message.error("删除失败，请重试");
+          throw e;
+        }
+      },
+    });
+    setDraftModalVisible(false);
+  };
 
   const handleSave = async () => {
     if (!form.title.trim()) {
-      Message.error('请输入文章标题');
+      Message.error("请输入文章标题");
       return;
     }
     if (!editor.getHTML().trim()) {
-      Message.error('请输入文章内容');
+      Message.error("请输入文章内容");
       return;
     }
     if (!form.category) {
-      Message.error('请选择文章分类');
+      Message.error("请选择文章分类");
       return;
     }
     if (!form.contentType) {
-      Message.error('请选择内容类型');
+      Message.error("请选择内容类型");
       return;
     }
-   await createArticleApi({...form,content:editor.getHTML()}).then((res) => {
-    if (res.data.status) {
-      Message.success('发布成功');
-    } else {
-      Message.error('发布失败');
-    }
-   })
+    await createArticleApi({ ...form, content: editor.getHTML() }).then(
+      (res) => {
+        if (res.data.status) {
+          Message.success("发布成功");
+        } else {
+          Message.error("发布失败");
+        }
+      }
+    );
     // TODO: 实现保存逻辑
   };
 
@@ -86,35 +163,55 @@ export default function ArticleEditPage() {
             <Input
               placeholder="输入文章标题..."
               value={form.title}
-              onChange={value => setForm(prev => ({ ...prev, title: value }))}
+              onChange={(value) =>
+                setForm((prev) => ({ ...prev, title: value }))
+              }
               className="title-input"
             />
-            <Tag color={isDraft ? 'gray' : 'arcoblue'} className="status-tag">
-              {isDraft ? '草稿' : '已发布'}
+            <Tag color={isDraft ? "gray" : "arcoblue"} className="status-tag">
+              {isDraft ? "草稿" : "已发布"}
             </Tag>
           </div>
           <div className="header-right">
-            <Button 
+            <Button
               type="secondary"
               icon={<IconSave />}
-              // onClick={() => handleSave(true)}
+              onClick={handleOpenDraftModal}
+            >
+              导入草稿
+            </Button>
+            <Button
+              type="secondary"
+              icon={<IconSave />}
+              onClick={() => {
+                toSaveDraft();
+                setTag(0);
+              }}
             >
               保存草稿
             </Button>
-            <Button 
+            <Button
               type="primary"
               icon={<IconSend />}
-              onClick={() => handleSave()}
+              onClick={() => {
+                handleSave();
+                if (lastId) {
+                  deleteDraft(lastId.current);
+                  setDrafts(
+                    drafts.filter((draft) => draft.id !== lastId.current)
+                  );
+                  lastId.current = null;
+                }
+              }}
             >
               发布文章
-            </Button> 
+            </Button>
           </div>
         </div>
       </header>
 
       <main className="editor-main bg-white">
-      
-      <div>
+        <div>
           <div className="mb-6">
             <div className="text-base font-medium text-gray-800 mb-4 pl-3 border-l-4 border-[#165DFF]">
               文章信息
@@ -127,9 +224,11 @@ export default function ArticleEditPage() {
                   className="w-full"
                   allowClear
                   value={form.category}
-                  onChange={value => setForm(prev => ({ ...prev, category: value }))}
+                  onChange={(value) =>
+                    setForm((prev) => ({ ...prev, category: value }))
+                  }
                 >
-                  {CATEGORY.map(({value, label}) => (
+                  {CATEGORY.map(({ value, label }) => (
                     <Select.Option key={value} value={value}>
                       {label}
                     </Select.Option>
@@ -143,13 +242,18 @@ export default function ArticleEditPage() {
                   className="w-full"
                   allowClear
                   value={form.contentType}
-                  onChange={value => setForm(prev => ({ ...prev, contentType: value }))}
+                  onChange={(value) =>
+                    setForm((prev) => ({ ...prev, contentType: value }))
+                  }
                 >
-                  {form.category && CONTENT_TYPE[form.category as keyof typeof CONTENT_TYPE].map((value) => (
-                    <Select.Option key={value} value={value}>
-                      {value}
-                    </Select.Option>
-                  ))}
+                  {form.category &&
+                    CONTENT_TYPE[
+                      form.category as keyof typeof CONTENT_TYPE
+                    ].map((value) => (
+                      <Select.Option key={value} value={value}>
+                        {value}
+                      </Select.Option>
+                    ))}
                 </Select>
               </div>
             </div>
@@ -162,7 +266,9 @@ export default function ArticleEditPage() {
                 allowCreate
                 allowClear
                 value={form.tags}
-                onChange={value => setForm(prev => ({ ...prev, tags: value }))}
+                onChange={(value) =>
+                  setForm((prev) => ({ ...prev, tags: value }))
+                }
               />
             </div>
             <div className="mb-4">
@@ -170,9 +276,11 @@ export default function ArticleEditPage() {
               <Input.TextArea
                 placeholder="请输入文章引言，将显示在文章列表中"
                 className="w-full"
-                style={{ minHeight: '100px' }}
+                style={{ minHeight: "100px" }}
                 value={form.excerpt}
-                onChange={value => setForm(prev => ({ ...prev, excerpt: value }))}
+                onChange={(value) =>
+                  setForm((prev) => ({ ...prev, excerpt: value }))
+                }
                 maxLength={200}
                 showWordLimit
               />
@@ -181,7 +289,7 @@ export default function ArticleEditPage() {
         </div>
         <MenuBar editor={editor} />
         <IeseEditor editor={editor} className="w-full h-full" />
-      {/* <div>
+        {/* <div>
         <div className="editor-toolbar">
           <Space size="small" className="format-tools">
             {formatTools.map((tool, index) => (
@@ -225,6 +333,69 @@ export default function ArticleEditPage() {
         </div>
         </div> */}
       </main>
+
+      <Modal
+        title="选择草稿"
+        visible={draftModalVisible}
+        onCancel={() => setDraftModalVisible(false)}
+        footer={null}
+        style={{ width: 600 }}
+      >
+        {drafts.length === 0 ? (
+          <div className="text-center py-10 text-gray-400">暂无草稿</div>
+        ) : (
+          <List
+            bordered={false}
+            dataSource={drafts}
+            render={(draft) => (
+              <List.Item
+                actions={[
+                  <Button
+                    type="text"
+                    onClick={() => handleImportDraft(draft.id)}
+                  >
+                    导入
+                  </Button>,
+                  <Button
+                    type="text"
+                    status="danger"
+                    onClick={() => handleDeleteDraft(draft.id)}
+                  >
+                    删除
+                  </Button>,
+                ]}
+              >
+                <List.Item.Meta
+                  title={
+                    <div className="flex items-center">
+                      <span className="font-medium mr-2">
+                        {draft.title || "无标题草稿"}
+                      </span>
+                    </div>
+                  }
+                  description={
+                    <div>
+                      <div className="text-xs text-gray-500">
+                        {
+                          //new Date(draft.id).toLocaleString()
+                          new Date(draft.id).toLocaleString("zh-CN", {
+                            timeZone: "Asia/Shanghai",
+                          })
+                        }
+                      </div>
+                      {draft.excerpt && (
+                        <div className="mt-1 text-gray-600 line-clamp-1">
+                          {draft.excerpt}
+                        </div>
+                      )}
+                    </div>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        )}
+      </Modal>
     </div>
   );
 }
